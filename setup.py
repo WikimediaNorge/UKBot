da537513c1a5772eaa76d0a64c304221319780dc
Jon Harald Søby <jhsoby@gmail.com>
2025-04-08 16:22:42 +0200

    Cleanup: Remove files that are no longer necessary

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os, sys
 
 setup(name='ukbot',
-      version='1.0.0',
+      version='1.1.0',
       description='Wikipedia writing contest bot',
       keywords='wikipedia',
       author='Dan Michael O. Heggø',
